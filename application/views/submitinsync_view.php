749d9dfed915ef23b05d978a653760ee8e576039
alexxxveli4ko-newsite <velichko@newsite.by>
2018-11-29 09:28:19 +0300

    Правки

--- application/views/submitinsync_view.php
+++ application/views/submitinsync_view.php
@@ -1,5 +1,5 @@
 <? include 'insync_view.php'; ?>
 <a id="submitInsync" href="insync://transfer?id=<?=$data["insync"]["id"]?>" style="display: none"></a>
 <script type="text/javascript">
-    // document.getElementById('submitInsync').click();
+    document.getElementById('submitInsync').click();
 </script>
