45e3b6ba3322b0f8328176ffd2f2900d404b0a8e
Olga Shalkevich <Volha.Shalkevich@alfa-bank.by>
2023-02-16 16:56:56 +0300

    feat: markup

--- application/views/submitinsync_view.php
+++
@@ -1,5 +0,0 @@
-<? include 'insync_view.php'; ?>
-<a id="submitInsync" href="insync://transfer?id=<?=$data["insync"]["id"]?>" style="display: none"></a>
-<script type="text/javascript">
-    document.getElementById('submitInsync').click();
-</script>
